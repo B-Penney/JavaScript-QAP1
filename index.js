cb874b80c3eb155f817c2b3865a1b24fca9c742d
Beth-Ann Penney <bethann.penney@gmail.com>
2024-09-19 23:15:18 -0230

    Add symbols feature to password generator

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
 // Beth-Ann Penney-Rideout
 // September 19, 2024
 
-// Function to generate a random password
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
 
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numberChars = "0123456789";
-    
+    const symbolChars = "!@#$%^&*()_+-=";
 
     let allowedChars = "";
     let password = "";
@@ -18,7 +17,7 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     allowedChars += includeLowercase ? lowercaseChars : "";
     allowedChars += includeUppercase ? uppercaseChars : "";
     allowedChars += includeNumbers ? numberChars : "";
-   
+    allowedChars += includeSymbols ? symbolChars : "";
 
     if(length <= 0){
         return `(password length must be at least 8 characters)`;
@@ -39,12 +38,12 @@ const passwordLength = 8;
 const includeLowercase = true;
 const includeUppercase = true;
 const includeNumbers = true;
-
+const includeSymbols = true;
 
 const password = generatePassword(passwordLength, 
                                  includeLowercase, 
                                  includeUppercase, 
                                  includeNumbers, 
-                                 
+                                 includeSymbols);
 
-console.log('Generated password: ${password}'));
+console.log(`Generated password: ${password}`);
